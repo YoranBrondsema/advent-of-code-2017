7db18e1197ed29667a99b58703ff82c7b9155873
Yoran Brondsema <yoran@sutori.com>
2017-12-28 15:48:43 -0500

    Day 23

--- day23.exs
+++ day23.exs
@@ -1,9 +1,19 @@
 defmodule Day23 do
+  def mul_count(input) do
+    input
+    |> String.trim
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.trim/1)
+    |> Day23.run
+    |> Enum.count(
+      fn(instruction) -> instruction == :mul end
+    )
+  end
+
   def run(instructions) do
     _run(instructions, Map.new, 0, [])
     |> Enum.reverse
   end
-
   defp _run(instructions, registry, pc, executed) do
     instruction = Enum.at(instructions, pc)
 
@@ -15,9 +25,22 @@ defmodule Day23 do
     end
   end
 
+  def run2(instructions, registry), do: _run2(instructions, registry, 0)
+  defp _run2(instructions, registry, pc) do
+    instruction = Enum.at(instructions, pc)
+
+    case instruction do
+      nil -> registry
+      _ ->
+        { registry, pc, _ } = process_instruction(instruction, registry, pc, [])
+        _run2(instructions, registry, pc)
+    end
+  end
+
+
   def process_instruction(instruction, registry, pc, executed) do
     [_, cmd, x, y] = ~r/(set|sub|mul|jnz) (.+) (.+)/
-               |> Regex.run(instruction)
+      |> Regex.run(instruction)
 
     case cmd do
       "set" -> process_set(registry, pc, executed, x, y)
@@ -86,14 +109,25 @@ defmodule Day23 do
       { value, _ } -> value
     end
   end
+
+  def value_of_h do
+    b = 106500
+    b..(b+17000)
+    |> Enum.take_every(17)
+    |> Enum.reject(&is_prime/1)
+    |> length
+  end
+
+  def is_prime(x) do
+    (2..x |> Enum.filter(fn a -> rem(x, a) == 0 end) |> length()) == 1
+  end
 end
 
 { :ok, input } = File.read "day23-input.txt"
 
-input
-|> String.split("\n", trim: true)
-|> Day23.run
-|> Enum.count(
-  fn(instruction) -> instruction == :mul end
-)
-|> IO.inspect
+mul_count = input
+            |> Day23.mul_count
+IO.puts "mul count: #{mul_count}"
+
+value_of_h = Day23.value_of_h
+IO.puts "value of h: #{value_of_h}"
